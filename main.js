0d970bde23e9fb4dd96f5a7d1029bbe8c2432937
Kyle <kyletherosenberg@icloud.com>
2017-02-18 16:44:50 -0700

    Loads and Prints Data

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-var reader = new FileReader();
-var file = new File([""], "pokemon.j");
-reader.onload = function(e) { 
-	var contents = e.target.result;
-};
-console.log("opening file " + file.name + ": " + file.size);
-reader.readAsText(file);
+var pokemonjsonurl = "https://raw.githubusercontent.com/KyleRosenberg/PokemonTeamBuilder/master/pokemon.j";
+var typejsonurl = "https://raw.githubusercontent.com/KyleRosenberg/PokemonTeamBuilder/master/types.j";
+var movesjsonurl = "https://raw.githubusercontent.com/KyleRosenberg/PokemonTeamBuilder/master/moves.j";
+
+var pokemonjson;
+var typejson;
+var movejson;
+
+$.ajax({ 
+	url: pokemonjsonurl, success: function(data) {
+		pokemonjson = JSON.parse(data);
+		printArray(pokemonjson);
+	} 
+});
+
+$.ajax({ 
+	url: typejsonurl, success: function(data) {
+		typejson = JSON.parse(data);
+		printArray(typejson);
+	} 
+});
+
+$.ajax({ 
+	url: movesjsonurl, success: function(data) {
+		movejson = JSON.parse(data);
+		printArray(movejson);
+	} 
+});
+
+function printArray(arr){
+	for (var i = 0; i<arr.length; i++){
+		document.write("<p>" + arr[i].name + ": " + (i+1) + "</p>");
+	}
+}
