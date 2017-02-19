80326216aaee38e96929aa60e5274c5950a46f5b
Kyle <kyletherosenberg@icloud.com>
2017-02-18 22:27:53 -0700

    Kind of added search feature

--- main.js
+++ main.js
@@ -9,26 +9,52 @@ var movejson;
 $.ajax({ 
 	url: pokemonjsonurl, success: function(data) {
 		pokemonjson = JSON.parse(data);
-		printArray(pokemonjson);
+		var $pokemon = $(".ui.search");
+
+		$pokemon
+			.search({
+				searchFields : ['name'],
+				source : pokemonjson,
+				onResults: function(response){
+					var html = '';
+     				if(response!== undefined) {
+        				$.each(response, function(index, result) {	
+        					for (var i = 0; i<result.length; i++){
+	        					r = result[i];
+	        					var imagelink = r.sprites.front_default;
+            					html  += '<a class="result"><div class="content">';
+            					html += '<img src=' + imagelink + '></img>';
+            					html += '<div class="title">' + r.name.charAt(0).toUpperCase() + r.name.slice(1) + '</div>';
+            					html  += '' + '</div></a>';
+            				}
+        				});
+        				$('.ui.search div.results').html(html);
+     				}
+      				return false;
+    			},
+    			onResultsAdd: function(html){
+        			if($('div.results a').length==0){
+            			return "default";
+       				}
+        			else return false;
+   				},
+   				onSelect: function(result, response){
+   					console.log(result);
+   					return false;
+   				}
+		});
 	} 
 });
 
 $.ajax({ 
 	url: typejsonurl, success: function(data) {
 		typejson = JSON.parse(data);
-		printArray(typejson);
 	} 
 });
 
 $.ajax({ 
 	url: movesjsonurl, success: function(data) {
 		movejson = JSON.parse(data);
-		printArray(movejson);
 	} 
 });
 
-function printArray(arr){
-	for (var i = 0; i<arr.length; i++){
-		document.write("<p>" + arr[i].name + ": " + (i+1) + "</p>");
-	}
-}
